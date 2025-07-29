4faf04caa152939390e32b0d92dc070aa9048007
itstudy001 <itstudy@kakao.com>
2025-07-30 06:27:03 +0900

    jenkinsgradle

---
+++ src/main/java/com/adamsoft/jenkinsgradle/JenkinsgradleApplication.java
@@ -0,0 +1,13 @@
+package com.adamsoft.jenkinsgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsgradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsgradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adamsoft/jenkinsgradle/JenkinsgradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamsoft.jenkinsgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsgradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
